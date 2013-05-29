cc2c084181013336afc9bd7b8a8ecb77175d105c
Evgeny Ezhov <ezhov.evgeny@gmail.com>
2013-05-29 18:36:05 +0400

    DEV::Added custom logger

---
+++ src/ru/ezhoff/geolocation/geoexplorer/ExplorerActivity.java
@@ -0,0 +1,64 @@
+package ru.ezhoff.geolocation.geoexplorer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ExplorerActivity extends Activity {
+
+    private Button startStopButton;
+    private Button markerButton;
+    private TextView outputView;
+    private FileLogger logger;
+
+    private boolean active = false;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        startStopButton = (Button) findViewById(R.id.startStopButton);
+        markerButton    = (Button) findViewById(R.id.markerButton);
+        outputView      = (TextView) findViewById(R.id.outputText);
+        FileLogger.setOutputView(outputView);
+        startStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeMonitoringState();
+            }
+        });
+        markerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logger.warn("MARKER");
+            }
+        });
+        FileLogger.getInstance().info("Loaded.");
+    }
+
+    private void changeMonitoringState() {
+        active = !active;
+        if (active) {
+            activateMonitoring();
+        } else {
+            deactivateMonitoring();
+        }
+        startStopButton.setText(active ? R.string.stop : R.string.start);
+    }
+
+    private void activateMonitoring() {
+        logger = FileLogger.getInstance();
+        //Activate modules here
+    }
+
+    private void deactivateMonitoring() {
+        //Deactivate modules here
+        logger.close();
+    }
+}
